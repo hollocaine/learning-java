5eb74bd2fe4e0e23b12b51253e5170e246442ce0
alan_flynn <alanflynn@eazysafe.com>
2021-04-15 16:47:33 +0100

    Reference types

--- src/com/hollocaine/Main.java
+++ src/com/hollocaine/Main.java
@@ -1,13 +1,19 @@
 package com.hollocaine;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
-	    //write your code here
-        /*
-        * here
-        * we are here
-        * */
-        System.out.println("Hello world");
+	    double help = 5.555;
+	    Date date = new Date();
+        System.out.println(date.toString());
+        String name = new String("Alan");
+        System.out.println(name.toUpperCase());
+        LocalDate now = LocalDate.now();
+        System.out.println(now.getMonth());
+
+
     }
 }
