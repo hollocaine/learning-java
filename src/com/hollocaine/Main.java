00de7d4e7313844c4979a222572f5ca1baffb41b
alan_flynn <alanflynn@eazysafe.com>
2021-04-15 17:00:34 +0100

    Primitives and reference types

--- src/com/hollocaine/Main.java
+++ src/com/hollocaine/Main.java
@@ -6,14 +6,32 @@ import java.util.Locale;
 public class Main {
 
     public static void main(String[] args) {
-	    double help = 5.555;
+	  /*  double help = 5.555;
 	    Date date = new Date();
         System.out.println(date.toString());
         String name = new String("Alan");
         System.out.println(name.toUpperCase());
         LocalDate now = LocalDate.now();
-        System.out.println(now.getMonth());
+        System.out.println(now.getMonth());*/
+        /*Primitive types*/
+        int a = 10;
+        int b = a;
+        a = 100;
+        System.out.println("a " + a + " - b " + b);
+        /*Reference types*/
+        Person alex = new Person("Alex");
+        Person miriam = alex;
+        System.out.println("Before changing alex");
+        System.out.println(alex.name + " " + miriam.name);
+        alex.name = "Alexander";
+        System.out.println("After changing alex");
+        System.out.println(alex.name + " " + miriam.name);
+    }
+    static class Person {
+        String name;
 
-
+        Person(String name) {
+            this.name = name;
+        }
     }
 }
